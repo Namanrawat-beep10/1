7c236f3e7572b51c8a866b5e8134c10e42bd4b42
Namanrawat-beep10 <namanrawat729@gmail.com>
2026-09-18 13:18:24 +0530

    Add files via upload

---
+++ day21-swappingofnumarr.c
@@ -0,0 +1,31 @@
+/*Q41.Write a program to swap the first and last digit of a number.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n, original, first, last, digits = 1, middle, result;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    last = n % 10;
+
+    while(n >= 10)
+    {
+        n = n / 10;
+        digits = digits * 10;
+    }
+
+    first = n;
+
+    middle = (original % digits) / 10;
+
+    result = last * digits + middle * 10 + first;
+
+    printf("After swapping = %d", result);
+
+    return 0;
+}
