d72ad3e7ed7c775fa6830556b7aba4538f1ed18f
Namanrawat-beep10 <namanrawat729@gmail.com>
2026-09-18 13:20:19 +0530

    Add files via upload

---
+++ day22-strongnum.c
@@ -0,0 +1,32 @@
+/*Q43.Write a program to check if a number is a strong number.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n, original, rem, i, fact, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while(n > 0)
+    {
+        rem = n % 10;
+        fact = 1;
+
+        for(i = 1; i <= rem; i++)
+            fact = fact * i;
+
+        sum = sum + fact;
+        n = n / 10;
+    }
+
+    if(sum == original)
+        printf("Strong Number");
+    else
+        printf("Not Strong Number");
+
+    return 0;
+}
